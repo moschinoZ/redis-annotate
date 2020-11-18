0f407cca0394ee2c63b452b21a676fe36c33ee22
朱佳明 <jiaming_zhu@qingsongchou.com>
2020-11-18 18:21:00 +0800

    first commit

---
+++ deps/hiredis/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zhujiaming/redis-6.0.4/deps/hiredis
+# Build directory: /Users/zhujiaming/redis-6.0.4/deps/hiredis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "/Users/zhujiaming/redis-6.0.4/deps/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "/Users/zhujiaming/redis-6.0.4/deps/hiredis/CMakeLists.txt;83;ADD_TEST;/Users/zhujiaming/redis-6.0.4/deps/hiredis/CMakeLists.txt;0;")
